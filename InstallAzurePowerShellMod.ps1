84b62da1f4cc606c4ba03856b92b069b60c6b85f
Nik Chris <nchristoforatos@gmail.com>
2020-06-03 09:42:20 +0000

    traffic manager setup

---
+++ InstallAzurePowerShellMod.ps1
@@ -0,0 +1,6 @@
+if (Get-Module -Name AzureRM -ListAvailable) {
+    Write-Warning -Message ('Az module not installed. Having both the AzureRM and ' +
+      'Az modules installed at the same time is not supported.')
+} else {
+    Install-Module -Name Az -AllowClobber -Scope CurrentUser
+}
