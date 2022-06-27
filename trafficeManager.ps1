55e80903dcdfbcfb7e8982723c014a307bfda2f4
Nikos Christoforatos <44776417+nchristoforatos@users.noreply.github.com>
2022-06-27 18:44:26 +0300

    Delete trafficeManager.ps1

--- trafficeManager.ps1
+++
@@ -1,7 +0,0 @@
-#Create Resource Group
-New-AzResourceGroup -Name TrafficManagerRG1 -Location 'West Europe'
-#Create Traffic Manager Profile
-$TmProfile = New-AzTrafficManagerProfile -Name TMPortal -ResourceGroupName TrafficManagerRG1 -TrafficRoutingMethod Priority -RelativeDnsName TMPortal -Ttl 10 `
--MonitorProtocol HTTPS -MonitorPort 443 -MonitorPath "/" 
-
-#Update Traffic Manager Profile
